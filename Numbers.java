b1612fcd2fc0f06d4529cbd5b9f508bb71ac0038
Dhinagar492 <115131623+Dhinagar492@users.noreply.github.com>
2022-10-06 09:54:30 +0530

    Add files via upload

---
+++ Numbers.java
@@ -0,0 +1,31 @@
+class Numbers{
+public static void main(String[]args)
+{
+int a=12;
+int b=-13;
+int c=25;
+String name="SAM";
+char initial='D';
+byte age=22;
+float height=167.6f;
+double weight=66.6;
+long salary=300000;
+boolean d1=true;
+boolean d2=false;
+System.out.println("a="+a+"\n"+"b="+b);
+System.out.println("name="+name);
+System.out.println("initial="+initial);
+System.out.println("age="+age);
+System.out.println("height="+height);
+System.out.println("weight="+weight);
+System.out.println("salary="+salary);
+if(a<b)
+{
+System.out.println(d1);
+}
+else
+{
+System.out.println(d2);
+}
+}
+}
